b3b12b39fc22a4f3e90ed28629b78fa899abf955
Савелий <savelussava@mail.ru>
2021-09-17 21:14:25 +0500

    First commit

---
+++ Algols/Algorithms.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algols
+{
+    public class Algorithms
+    {
+        public void ConstantFunction(int[] vector)
+        {
+            Console.Write(' ');
+        }
+
+        public void SumOfElements(int[] vector)
+        {
+            int sum = 0;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                sum += vector[i];
+            }
+        }
+
+        public void ProductOfElements(int[] vector)
+        {
+            int sum = 0;
+            for (int i = 0; i < vector.Length; i++)
+            {
+                sum *= vector[i];
+            }
+        }
+
+        public void StandartCalculatePolynomial(int[] vector)
+        {
+            double sum = 0;
+            for (int i = 0; i < vector.Length - 1; i++)
+            {
+                sum += vector[i] * Math.Pow(1.5, i);
+            }
+        }
+
+        public double GornerCalculatePolynomial(int[] vector)
+        {
+            double sum = 0;
+            if (vector.Length != 0)
+            {
+                sum += vector[0] + 1.5 * GornerCalculatePolynomial();
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
---
+++ Algols/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace Algols
+{
+    class Program
+    {
+        public static Stopwatch Watch = new Stopwatch();
+        public static Algorithms algorithms = new Algorithms();
+        static int[] CreateVector(int length)
+        {
+            var rnd = new Random(42);
+            var Vector = new int[length];
+            for (int i = 0; i < length; i++)
+            {
+                Vector[i] = rnd.Next();
+            }
+            return Vector;
+        }
+
+        public static void Main(string[] args)
+        {
+            
+            List<int> timeResults = new List<int>();
+            for (int i = 0; i < 2000; i++)
+            {
+                var vector = CreateVector(i);
+                Watch.Reset();
+                Watch.Start();
+                algorithms.StandartCalculatePolynomial(vector);
+                Watch.Stop();
+                timeResults.Add((int)Watch.ElapsedTicks);
+            }
+            FillFile(timeResults);
+        }
+
+        public static void FillFile(List<int> timeResults)
+        {
+            File.WriteAllText("Data.csv", CreateLine(timeResults));
+        }
+
+        public static string CreateLine (List<int> timeResults)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var result in timeResults)
+            {
+                sb.Append(result + ";");
+            }
+
+            return sb.ToString();
+        }
+
+
+    }
+}

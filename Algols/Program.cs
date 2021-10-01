bc36bd8e5ccc8a9bcb68e04ab9d5aab904aa584e
Савелий <savelussava@mail.ru>
2021-10-01 13:44:58 +0500

    add algorithms

--- Algols/Program.cs
+++ Algols/Program.cs
@@ -12,55 +12,66 @@ namespace Algols
     {
         public static Stopwatch Watch = new Stopwatch();
         public static Algorithms algorithms = new Algorithms();
-        static int[] CreateVector(int length)
+        public static Random rnd = new Random();
+
+        private static int[] CreateVector(int length)
         {
             var Vector = new int[length];
             for (int i = 0; i < length; i++)
             {
-                Vector[i] = 2;
+                Vector[i] = rnd.Next(0,2000);
             }
             return Vector;
         }
 
+        private static int[,] CreateMatrix(int height, int width)
+        {
+            var matrix = new int[height, width];
+            for (int i = 0; i < height; i++)
+            {
+                for (int j = 0; j < width; j++)
+                {
+                    matrix[i, j] = rnd.Next(1, 10);
+                }
+            }
+            return matrix;
+        }
         public static void Main(string[] args)
         {
-            double x1 = 0;
-            double x = 0;
-            List<int> timeResults = new List<int>();
-            for (int i = 0; i < 1980; i++)
+
+            List<int>[] timeResultsList = new List<int>[5];
+            for (int k = 0; k < timeResultsList.Length; k++)
             {
-                var vector = CreateVector(i);
-
-                //List<int> temp = new List<int>();
-                //for (int j = 0; j < 5; j++)
-                //{
-                //    Watch.Reset();
-                //    Watch.Start();
-                //    algorithms.SumOfElements(vector);
-                //    Watch.Stop();
-                //    temp.Add((int)Watch.ElapsedTicks);
-                //}
-                //timeResults.Add(temp.Min());
-
-                Watch.Reset();
-                Watch.Start();
-                int[] xs = algorithms.QuickSort(vector);
-                Watch.Stop();
-                timeResults.Add((int)Watch.ElapsedTicks);
-
+                List<int> timeResults = new List<int>();
+                for (int i = 0; i < 500; i++)
+                {
+                    //var vector = CreateVector(i);
+                    int[,] matrix1 = CreateMatrix(i, i);
+                    int[,] matrix2 = CreateMatrix(i, i);
+                    Watch.Reset();
+                    Watch.Start();    
+                    double x = algorithms.MultiplyMatrix(matrix1, matrix2);
+                    Watch.Stop();
+                    timeResults.Add((int)Watch.ElapsedTicks);
+                }
+                timeResultsList[k] = timeResults;
             }
-            //FillFile(timeResults);
-            FillFile(RemoveEmissions(timeResults));
+            FillFile(RemoveEmissions(timeResultsList));
         }
-        public static List<int> RemoveEmissions(List<int> vector)
+        public static List<int> RemoveEmissions(List<int>[] timeResultsList)
         {
-            var vectorArr = vector.ToArray();
-            for (int i = 1; i < vectorArr.Length - 1; i++)
+            var endVector = Enumerable.Repeat(int.MaxValue, timeResultsList[0].Count).ToArray();
+            for (int i = 0; i < timeResultsList[0].Count; i++)
             {
-                if (vectorArr[i] >= vectorArr[i - 1] * 1.5 && vectorArr[i] >= vectorArr[i + 1] * 1.5)
-                    vectorArr[i] = vectorArr[i - 1];
+                for (int j = 0; j < timeResultsList.Length; j++)
+                {
+                    if (endVector[i] > timeResultsList[j][i])
+                    {
+                        endVector[i] = timeResultsList[j][i];
+                    }
+                }
             }
-            return vectorArr.ToList();
+            return endVector.ToList();
         }
         public static void FillFile(List<int> timeResults)
         {
